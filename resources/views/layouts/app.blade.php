ecbe4bcf28ee44358825baefd67af80144eaa554
a2175 <as217512@gmail.com>
2020-07-11 23:25:18 +0900

    LaravelProject 1.00v

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/common.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet">
+</head>
+<body>
+    <header id="header">
+        <div>
+            <div id="logo">
+                <h3><a href="/">PHP MVC MODEL HOMPAGE</a></h3>
+            </div>
+        </div>
+        <nav id="gnb">
+            <ul>
+                <li><a href="/">게시판</a></li>
+                <li><a href="/">채팅</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    @yield('content')
+
+    <footer id="footer">
+        copyright (c) 2017 Junil-hwang all right reserved.
+    </footer>
+</body>
+</html>
